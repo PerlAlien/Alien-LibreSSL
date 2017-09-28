33de9c974809d2fe133ac97b0765040745aba6ed
Graham Ollis <plicease@cpan.org>
2017-09-28 15:25:27 -0400

    Initial commit

---
+++ lib/Alien/LibreSSL.pm
@@ -0,0 +1,11 @@
+package Alien::LibreSSL;
+
+use strict;
+use warnings;
+use 5.008001;
+use base qw( Alien::Base );
+
+# ABSTRACT: Alien wrapper for LibreSSL (alternative to OpenSSL)
+# VERSION
+
+1;
